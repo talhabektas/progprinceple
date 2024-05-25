4688479445ab2652a0e037ded4539bb42e32fc5f
talhabektas <mehmettalha.bektas@gmail.com>
2024-05-25 23:20:35 +0300

    committed

--- src/StarPattern.java
+++ src/StarPattern.java
@@ -3,10 +3,15 @@ public class StarPattern extends Pattern {
     public String generate(int size) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < size; i++) {
-            int stars = 2 * i + 1;
-            sb.append(" ".repeat(size - i - 1));
-            sb.append("* ".repeat(stars));
-            sb.append("\n");
+            // Boşlukları ekle
+            for (int j = 0; j < size - i - 1; j++) {
+                sb.append("  ");
+            }
+            // Yıldızları ekle
+            for (int k = 0; k < 2 * i + 1; k++) {
+                sb.append("* ");
+            }
+            sb.append("\n"); // Satır sonunu ekle
         }
         return sb.toString();
     }
